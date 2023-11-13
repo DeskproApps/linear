b1930aebfc649a16e0776e327861c4e8adc81072
ilia makarov <ilia.makarov@me.com>
2023-11-13 10:41:34 +0200

    Added icon to Team field

---
+++ src/components/common/Team/icons.ts
@@ -0,0 +1,37 @@
+import {
+  faCrown,
+  faUserGroup,
+  faSatelliteDish,
+  faGamepad,
+  faBatteryFull,
+  faVolumeOff,
+  faDatabase,
+  faTerminal,
+  faClock,
+  faStopwatch,
+  faUnlockKeyhole,
+  faCompass,
+  faServer,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGitlab } from "@fortawesome/free-brands-svg-icons";
+
+const icons = {
+  crown: faCrown,
+  gitlab: faGitlab,
+  team: faUserGroup,
+  radar: faSatelliteDish,
+  chip: undefined,
+  joystick: faGamepad,
+  battery: faBatteryFull,
+  sound: faVolumeOff,
+  cone: undefined,
+  database: faDatabase,
+  terminal: faTerminal,
+  watch: faClock,
+  clock: faStopwatch,
+  lock: faUnlockKeyhole,
+  compass: faCompass,
+  server: faServer,
+};
+
+export { icons };
